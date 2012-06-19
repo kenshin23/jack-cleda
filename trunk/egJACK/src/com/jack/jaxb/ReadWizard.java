af0bacae92f361554c65462b13b9c14daf0e0e30
zumkar8@gmail.com <zumkar8@gmail.com@d1d3a94f-aec1-933d-91e4-1d8fa642f385>
2012-06-19 05:20:00 +0000

    Read Wizard

---
+++ trunk/egJACK/src/com/jack/jaxb/ReadWizard.java
@@ -0,0 +1,38 @@
+package com.jack.jaxb;
+
+import java.io.InputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
+import com.jack.crud.xml.*;
+
+
+
+public class ReadWizard {
+
+  public static void main(String[] args) throws Exception {
+
+    // ----------------------------------------
+    // Obtener el InputStream
+    // ----------------------------------------
+
+    InputStream is = ClassLoader.getSystemResourceAsStream("com/jack/jaxb/wizardTest.xml");
+
+    // ----------------------------------------
+    // Inicializar JAXB y leer el XML
+    // ----------------------------------------
+
+   
+    JAXBContext jc = JAXBContext.newInstance(ObjectFactory.class.getPackage().getName());
+
+    Unmarshaller unmarshaller = jc.createUnmarshaller();
+
+    WIZARD xmlWizard = (WIZARD) unmarshaller.unmarshal(is);
+    
+    System.err.println("Nombre del Wizard: " + xml.getName());
+    
+        
+  }
+  
+}
