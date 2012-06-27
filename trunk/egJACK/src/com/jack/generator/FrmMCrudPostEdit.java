764a9dcf1aa9d9073bc3879bb1257cda74b04015
arloznav.sis@gmail.com <arloznav.sis@gmail.com@d1d3a94f-aec1-933d-91e4-1d8fa642f385>
2012-06-27 15:20:37 +0000

    crud generator. <modified>

---
+++ trunk/egJACK/src/com/jack/generator/FrmMCrudPostEdit.java
@@ -0,0 +1,111 @@
+package com.minotauro.sandbox.gui.Mpost;
+
+import nextapp.echo.app.Extent;
+
+import com.minotauro.echo.base.EnumEditMode;
+import com.minotauro.echo.cleda.list.var.FrmListCrudBase;
+import com.minotauro.echo.filter.base.FrmFilterEditor;
+import com.minotauro.echo.table.base.ETable;
+import com.minotauro.echo.table.base.TableColModel;
+import com.minotauro.echo.table.base.TableColumn;
+import com.minotauro.echo.table.renderer.LabelCellRenderer;
+import com.minotauro.query.QueryCreator;
+import com.minotauro.query.bean.gui.TextFilterBean;
+import com.minotauro.sandbox.model.Mpost;
+import com.minotauro.sandbox.model._PropMpost;
+// parametrizar paquetes con xml y xsd
+
+/**
+ * @author Por Ejemplo Jack Bauer
+ */
+public class FrmpostList extends FrmListCrudBase {
+
+  public FrmMpostList() {
+    frmEditDtaClass = FrmMpostEdit.class;
+    setTitle(_I18NFrmMpostList.title());
+  }
+
+  // --------------------------------------------------------------------------------
+
+  @Override
+  protected TableColModel initTableColModel() {
+    TableColModel tableColModel = new TableColModel();
+
+    TableColumn tableColumn;
+
+    // ----------------------------------------
+    // CheckBox
+    // ----------------------------------------
+
+    tableColModel.getTableColumnList().add( //
+        initTableSelColumn(editMode != EnumEditMode.SELECT, //
+            true, new Extent(5, Extent.PERCENT)));
+
+    // ----------------------------------------
+    // Name
+    // ----------------------------------------
+
+    tableColumn = new TableColumn() {
+      public Object getValue(ETable table, Object element) {
+        return ((Mpost) element).getName();
+      }
+    };
+    tableColumn.setWidth(new Extent(85, Extent.PERCENT));
+    tableColumn.setDataCellRenderer(new LabelCellRenderer());
+    tableColumn.setHeadCellRenderer(new LabelCellRenderer());
+    tableColumn.setHeadValue("Name");
+    tableColModel.getTableColumnList().add(tableColumn);
+
+    // ----------------------------------------
+    // Action
+    // ----------------------------------------
+
+    tableColModel.getTableColumnList().add( //
+        initTableCommandColumn(new Extent(10, Extent.PERCENT)));
+
+    // ----------------------------------------
+
+    return tableColModel;
+  }
+
+  // --------------------------------------------------------------------------------
+
+  @Override
+  protected void initFrmFilterEditor() {
+    frmFilterEditor = new FrmFilterEditor();
+    frmFilterEditor.addFilterListener(this);
+
+    frmFilterEditor.addFilter(_PropMpost.NAME, //
+        _I18NFrmMpostList.name(), //
+        TextFilterBean.class);
+
+    // ------------------------------------
+    // TODO: It's using sql column name :(
+    // ------------------------------------
+
+    frmFilterEditor.addFilter("odesc", //
+        _I18NFrmMpostList.desc(), //
+        TextFilterBean.class);
+  }
+
+  // --------------------------------------------------------------------------------
+
+  @Override
+  protected void initQueryCreator() {
+    queryCreator = new QueryCreator(Mpost.class, true);
+
+    // ----------------------------------------
+    // TODO: improve ordering
+    // ----------------------------------------
+
+    queryCreator.setOrderBy(_PropMpost.NAME);
+  }
+
+  // --------------------------------------------------------------------------------
+
+  @Override
+  protected Mpost initMBase() {
+    return new Mpost();
+  }
+}
+
