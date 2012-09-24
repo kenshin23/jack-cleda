a1e6972261cf55d35371cd99cb5bd34db03258e0
eaglegranlin@gmail.com <eaglegranlin@gmail.com@d1d3a94f-aec1-933d-91e4-1d8fa642f385>
2012-09-24 04:50:30 +0000

    el commit

---
+++ egJACK/hola2.java
@@ -0,0 +1,131 @@
+
+
+package com.minotauro.sandbox.gui.mcrudpost;
+
+import java.util.List;
+import nextapp.echo.app.Extent;
+import nextapp.echo.app.TextField;
+
+import com.minotauro.echo.beans.EFieldLabel;
+import com.minotauro.echo.beans.ETextAreaEx;
+import com.minotauro.echo.cleda.edit.FrmEditBase;
+import com.minotauro.echo.grid.FieldModel;
+import com.minotauro.echo.cleda.list.var.EJointButton;
+import com.minotauro.echo.cleda.list.var.EInnerButton;
+import com.minotauro.echo.cleda.list.var.EJointModel;
+import com.minotauro.echo.grid.SectionModel;
+import com.minotauro.echo.validator.impl.DuplicatedValidator;
+import com.minotauro.echo.validator.impl.NotEmptyValidator;
+import com.minotauro.echo.validator.impl.ConditionalValidator;
+import com.minotauro.echo.validator.impl.IntegerValidator;
+
+import com.minotauro.sandbox.gui.*;
+import com.minotauro.sandbox.gui.minnerpostc.FrmMInnerPostList;
+import com.minotauro.sandbox.gui.mmultjointmposta.FrmMMultJointMPostAList;
+import com.minotauro.sandbox.gui.msingjointpostb.FrmMSingJointPostBList;
+import com.minotauro.sandbox.model.*;
+
+//TODO:plantilla agregar dinamicamente imports.
+
+
+
+
+
+public class PnlWizardMCrudPost3 extends PnlWizardBase {
+
+  
+  protected FieldModel fmMMultJointMPostA;
+  protected FieldModel fmMSingJointPostB;
+
+  // --------------------------------------------------------------------------------
+
+  public PnlMCrudPostEdit() {
+    // Empty
+  }
+
+  // --------------------------------------------------------------------------------
+
+  @Override
+  protected void initGUI() {
+    String name = ((MCrudPost) data).getName(); 
+    setTitle(_I18NFrmMCrudPostEdit.title(name == null ? "-" : name)); 
+
+    // --------------------------------------------------------------------------------
+
+    SectionModel sectionModel = formModel.initSingleSectionModel();
+
+    // --------------------------------------------------------------------------------
+
+
+	sectionModel.addChild(fmMMultJointMPostA = initjointAB());
+	sectionModel.addChild(fmMSingJointPostB = initjointPostB());
+	
+	}
+  protected FieldModel initjointAB() {
+
+	    EJointModel jointModel = new EJointModel();
+
+		
+	    jointModel.init(
+	        data,
+	        MMultJointMPostA.class,
+	        _PropMCrudPost.MULT_JOINT_MPOST_ALIST,
+	        _PropMMultJointMPostA.CRUD_POST_REF,
+	        _PropMMultJointMPostA.CRUD_AREF);
+
+	    EJointButton jntJoint = new EJointButton(
+	    		FrmMMultJointMPostAList.class, jointModel, this);
+
+	    jntJoint.setEditMode(editMode);
+
+	    FieldModel fmJoint = new FieldModel();
+	    fmJoint.setLabelCmp(new EFieldLabel(_I18NFrmMCrudPostEdit.jointAB())); //TODO: editar properties
+	    fmJoint.setFieldCmp(jntJoint);
+	    fmJoint.setKey(_PropMCrudPost.MULT_JOINT_MPOST_ALIST);
+
+	    return fmJoint;
+	  }
+  
+  
+  
+  
+  protected FieldModel initjointPostB() {
+
+	    EJointModel jointModel = new EJointModel();
+		
+		
+	   	jointModel.init(
+	        data,
+	        MSingJointPostB.class,
+	        _PropMCrudPost.SING_JOINT_POST_BLIST,
+	        _PropMSingJointPostB.CRUD_POST_REF,
+	        _PropMSingJointPostB.CRUD_BREF);
+	        
+	        
+	    EJointButton jntJoint = new EJointButton(
+        		FrmMSingJointPostBList.class, jointModel, this) {
+
+      	protected String getInfoText() {
+        List<MSingJointPostB> aux =
+            ((MCrudPost) data).getSingJointPostBList();
+
+        return !aux.isEmpty()
+            ? aux.get(0).getCrudBRef().getName() +
+                " / " + aux.get(0).getName() //getname por defecto por ahora
+            : null;
+      		}
+    	};
+
+	    
+	    jntJoint.setEditMode(editMode);
+
+	    FieldModel fmJoint = new FieldModel();		
+	    fmJoint.setLabelCmp(new EFieldLabel(_I18NFrmMCrudPostEdit.jointPostB())); //TODO: editar properties
+	    fmJoint.setFieldCmp(jntJoint);
+	    fmJoint.setKey(_PropMCrudPost.SING_JOINT_POST_BLIST);
+
+	    return fmJoint;
+	  }
+  
+  
+}
