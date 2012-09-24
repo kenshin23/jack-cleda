115f3b92f4aa71ccc9c4b09cae34a19b7111ac70
arloznav.sis@gmail.com <arloznav.sis@gmail.com@d1d3a94f-aec1-933d-91e4-1d8fa642f385>
2012-09-24 04:18:45 +0000

    DmultiJointSmsB

---
+++ CledaSandbox/src/com/minotauro/sandbox/gui/dmultjointsmsb/_CledaI18N.java
@@ -0,0 +1,17 @@
+/*
+ * Created on 24/12/2006
+ */
+package com.minotauro.sandbox.gui.dmultjointsmsb;
+
+import com.minotauro.i18n.base.BaseI18NMain;
+
+/**
+ * @author E.G. Jack™
+ */
+public class _CledaI18N extends BaseI18NMain {
+
+  public static void main(String[] args) throws Exception {
+    _CledaI18N d = new _CledaI18N();
+    d.i18n(FrmDMultJointSmsBList.class.getSimpleName());
+  }
+}
