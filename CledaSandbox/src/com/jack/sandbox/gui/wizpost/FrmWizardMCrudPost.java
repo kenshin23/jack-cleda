a33aa8686ff82c17edb6801401a6d0cca49275ce
zumkar8@gmail.com <zumkar8@gmail.com@d1d3a94f-aec1-933d-91e4-1d8fa642f385>
2012-09-26 08:51:41 +0000

    menu wizard

--- CledaSandbox/src/com/jack/sandbox/gui/wizpost/FrmWizardMCrudPost.java
+++ CledaSandbox/src/com/jack/sandbox/gui/wizpost/FrmWizardMCrudPost.java
@@ -1,6 +1,4 @@
-/*
- * Created on 10/06/2012
- */
+
 package com.jack.sandbox.gui.wizpost;
 
 import com.minotauro.echo.cleda.wizard.FrmWizardDta;
@@ -9,36 +7,36 @@ import com.minotauro.echo.util.gui.MessageBox;
 import com.minotauro.echo.util.gui._I18NMessageBox;
 import com.minotauro.sandbox.model.MCrudPost;
 
-/**
- * @author e.g.:Jack
- */
-public class FrmWizardMPost extends FrmWizardDta {
+//TODO:plantilla agregar dinamicamente imports.
 
-  public FrmWizardMPost() {
-    super(_I18NFrmWizardMPost.title());
+
+public class FrmWizardMCrudPost extends FrmWizardDta {
+
+  // --------------------------------------------------------------------------------
+
+  public FrmWizardMCrudPost() {
+    super(_I18NFrmWizardMCrudPost.title());
   }
 
   // --------------------------------------------------------------------------------
 
-  @Override
+ @Override
   protected void initGUI() {
-    pnlWizardBaseList.add(new PnlWizardMPost1(this));
-    pnlWizardBaseList.add(new PnlWizardMPost2(this));
-    pnlWizardBaseList.add(new PnlWizardMPost3(this));
-    pnlWizardBaseList.add(new PnlWizardMPost4(this));
-    pnlWizardBaseList.add(new PnlWizardMPost5(this));
-    pnlWizardBaseList.add(new PnlWizardMPost6(this));
-    
+    pnlWizardBaseList.add(new PnlWizardMCrudPost1(this));
+    pnlWizardBaseList.add(new PnlWizardMCrudPost2(this));
+    pnlWizardBaseList.add(new PnlWizardMCrudPost3(this));
+    pnlWizardBaseList.add(new PnlWizardMCrudPost4(this));
+   
   }
-
-  // --------------------------------------------------------------------------------
+  
+// --------------------------------------------------------------------------------
 
   @Override
   protected void doFnshClicked(PnlWizardBase pnlWizardBase) {
     super.doFnshClicked(pnlWizardBase);
 
     MessageBox.msgBox(_I18NMessageBox.information(),
-        _I18NFrmWizardMPost.finalMessage(), null,
+        _I18NFrmWizardMCrudPost.finalMessage(), null,
         250, 100, MessageBox.OK_OPT);
   }
 
--- CledaSandbox/src/com/minotauro/sandbox/gui/dmainsms/_CledaI18N.java
+++ CledaSandbox/src/com/minotauro/sandbox/gui/dmainsms/_CledaI18N.java
@@ -1,15 +1,20 @@
+// ----------------------------------------
+// Generated code, do not edit
+// ----------------------------------------
+
+
 package com.minotauro.sandbox.gui.dmainsms;
 
 import com.minotauro.i18n.base.BaseI18NMain;
 
 /**
- * @author Karla Moreno
+ * @author E.G JACK™
  */
 public class _CledaI18N extends BaseI18NMain {
 
   public static void main(String[] args) throws Exception {
-    _CledaI18N d = new _CledaI18N();
-    d.i18n(FrmDMainSmsEdit.class.getSimpleName());
-    d.i18n(FrmDMainSmsList.class.getSimpleName());
+    _CledaI18N m = new _CledaI18N();
+    m.i18n(FrmDMainSmsEdit.class.getSimpleName());
+    m.i18n(FrmDMainSmsList.class.getSimpleName());
   }
 }
